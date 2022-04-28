97c6f5a756198b81d3690a22e06c08b5026d5ee8
Vitor Gabriel <59321138+Ranbut@users.noreply.github.com>
2022-04-27 22:12:30 -0300

    update script

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 function convertBinary() {
     binary = document.getElementById("binary_value").value;
-    var decimal = parseInt(binary, 2);
-    document.getElementById("decimal_value").innerHTML = "Decimal: "  + decimal;
+    if(binary == ""){
+      document.getElementById("decimal_value").innerHTML = "Insert a value (in binary obviously) and i will exibit here.";
+    }
+    else{
+      for (let i = 0; i < binary.length; i++) {
+        if (binary[i] == "0" || binary[i] == "1") {
+          if (isNaN(binary) != true){
+            var decimal = parseInt(binary, 2);
+            document.getElementById("decimal_value").innerHTML = "WOW! " + "The decimal value is: "  + decimal;
+          }
+        } 
+        else {
+          document.getElementById("decimal_value").innerHTML = "Sorry buddy, binary value contain only 0 and 1.";
+        }
+      }
+    }
   }
